583bfec7d4ce00e7a7ed9feb20cc4f21c855fc65
joigno <30682875+joigno@users.noreply.github.com>
2022-08-30 01:55:18 +0200

    Setting Delegator Trie Root and Clearing It (#3)
    * onlyOwner new root
    
    * slither fixes
    
    * branch initial
    
    * import MerkleProof.sol
    
    * branch initial tests sample
    
    * package merkletreejs,keccak256
    
    * no keccak256 sep function
    
    * emit delegator event
    
    * sender, not sender, basic tests
    
    * delegation clearing
    
    * package lock for merkletreejs
    
    * slither fixes

---
+++ contracts/MerkleDelegation.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+struct DelegatorRecord {
+    bytes32 trieRoot;
+    uint256 blockNumber;
+}
+
+struct DelegateeRecord {
+    address delegatee;
+    uint256 percentage; // 10,000 is 100%, 1,000 is 1%.
+}
+
+contract MerkleDelegation {
+    address public oracle; //TODO: use AccessControl
+    address public owner;
+    mapping(address => DelegatorRecord) public delegation;
+    mapping(address => address) public delegateToDelegator;
+
+    event OwnerInitialized(address owner);
+    event OwnerChanged(address newOwner);
+    event SetDelegates(address indexed delegator, bytes32 indexed trieRoot);
+    event ClearDelegate(address indexed delegator, bytes32 trieRoot);
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+    modifier onlyOracle() {
+        require(oracle == msg.sender, "Access: caller is not the oracle");
+        _;
+    }
+
+    constructor(address _oracle) {
+        require(_oracle != address(0), "DR: oracle must be non-zero");
+        owner = msg.sender;
+        oracle = _oracle;
+        emit OwnerInitialized(owner);
+    }
+
+    function changeOwner(address newOwner) external virtual onlyOwner {
+        require(newOwner != address(0), "DR: newOwner must be non-zero");
+        owner = newOwner;
+        emit OwnerChanged(owner);
+    }
+
+    function setDelegateTrie(address delegator, bytes32 trieRoot) external {
+        require(delegator != address(0), "DR: delegator must be non-zero");
+        require(trieRoot != bytes32(0), "DR: trieRoot must be non-zero");
+        require(msg.sender == delegator, "DR: delegator must be msg.sender");
+        // Record to storage.
+        delegation[delegator].trieRoot = trieRoot;
+        delegation[delegator].blockNumber = block.number;
+        emit SetDelegates(delegator, trieRoot);
+    }
+
+    function clearDelegateTrie(address delegator) external {
+        require(msg.sender == delegator, "DR: delegator must be msg.sender");
+        // Remove delegator record to save storage gas.
+        delete delegation[delegator];
+    }
+
+    function getDelegateRoot(address delegator)
+        external
+        view
+        returns (bytes32 trieRoot)
+    {
+        require(delegator != address(0), "DR: delegator must be non-zero");
+        return delegation[delegator].trieRoot;
+    }
+
+    function getDelegateBlockNumber(address delegator)
+        external
+        view
+        returns (uint256 blockNumber)
+    {
+        require(delegator != address(0), "DR: delegator must be non-zero");
+        return (delegation[delegator].blockNumber);
+    }
+}
---
+++ test/merkledelegation.test.ts
@@ -0,0 +1,162 @@
+// Start - Support direct Mocha run & debug
+import 'hardhat'
+import '@nomiclabs/hardhat-ethers'
+// End - Support direct Mocha run & debug
+
+import {MerkleTree} from 'merkletreejs'
+
+import chai, {expect} from 'chai'
+import {before} from 'mocha'
+import {solidity} from 'ethereum-waffle'
+import {MerkleDelegation} from '../typechain-types'
+import {deployContract, signer} from './framework/contracts'
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers'
+import {successfulTransaction} from './framework/transaction'
+import {eventOf} from './framework/event-wrapper'
+import {expectEmittersAndEvents, expectEvents} from './framework/event-filters'
+import {utils, BigNumber, constants} from 'ethers'
+
+// Wires up Waffle with Chai
+chai.use(solidity)
+
+/*
+ * The below comments are only for explaining the test layout.
+ *
+ * Actual tests do not need them, instead should practice code as documentation.
+ */
+
+// Start with the contract name as the top level descriptor
+describe('MerkleDelegation', () => {
+    /*
+     * Once and before any test, get a handle on the signer and observer
+     * (only put variables in before, when their state is not affected by any test)
+     */
+    before(async () => {
+        admin = await signer(0)
+        oracle = await signer(1)
+        delegator = await signer(2)
+        delegator2 = await signer(3)
+    })
+
+    // Before each test, deploy a fresh box (clean starting state)
+    beforeEach(async () => {
+        md = await deployContract<MerkleDelegation>(
+            'MerkleDelegation',
+            oracle.address
+        )
+    })
+
+    // Inner describes use the name or idea for the function they're unit testing
+    describe('changeOwner', () => {
+        /*
+         * Describe 'it', what unit of logic is being tested
+         * Keep in mind the full composition of the name: Contract > function > single test
+         */
+        it('same owner', async () => {
+            const newOwner = admin.address
+
+            const receipt = await successfulTransaction(
+                md.changeOwner(admin.address)
+            )
+
+            eventOf(md, 'OwnerChanged').expectOne(receipt, {newOwner})
+
+            expect(await md.owner()).equals(admin.address)
+        })
+
+        // Modifier checks contain the flattened and spaced modifier name
+        it('only owner', async () => {
+            await expect(
+                md.connect(oracle).changeOwner(oracle.address)
+            ).to.be.revertedWith('Ownable: caller is not the owner')
+        })
+    })
+
+    describe('setDelegateTrie', () => {
+        it('delegator is not sender', async () => {
+            const trieRoot = utils.soliditySha256(['string'], ['some input'])
+            await expect(
+                md
+                    .connect(delegator2)
+                    .setDelegateTrie(delegator.address, trieRoot)
+            ).to.be.revertedWith('DR: delegator must be msg.sender')
+        })
+
+        it('delegator is sender', async () => {
+            const trieRoot = utils.soliditySha256(['string'], ['some input'])
+
+            const receipt = await successfulTransaction(
+                md
+                    .connect(delegator)
+                    .setDelegateTrie(delegator.address, trieRoot)
+            )
+
+            eventOf(md, 'SetDelegates').expectOne(receipt, {
+                delegator: delegator.address,
+                trieRoot
+            })
+
+            expect(await md.owner()).equals(admin.address)
+        })
+
+        // Modifier checks contain the flattened and spaced modifier name
+        it('only owner', async () => {
+            await expect(
+                md.connect(oracle).changeOwner(oracle.address)
+            ).to.be.revertedWith('Ownable: caller is not the owner')
+        })
+
+        it('delegator update', async () => {
+            const trieRoot = utils.soliditySha256(['string'], ['some input'])
+            const blockNumber = BigNumber.from('0x28')
+            const receipt = await successfulTransaction(
+                md
+                    .connect(delegator)
+                    .setDelegateTrie(delegator.address, trieRoot)
+            )
+
+            expect(await md.getDelegateRoot(delegator.address)).equals(trieRoot)
+            expect(await md.getDelegateBlockNumber(delegator.address)).equals(
+                blockNumber
+            )
+        })
+    })
+
+    describe('clearDelegateTrie', () => {
+        it('delegator clearing', async () => {
+            const trieRoot = utils.soliditySha256(['string'], ['some input'])
+            const blockNumber = BigNumber.from('0x2a')
+            const receipt = await successfulTransaction(
+                md
+                    .connect(delegator)
+                    .setDelegateTrie(delegator.address, trieRoot)
+            )
+            expect(await md.getDelegateRoot(delegator.address)).equals(trieRoot)
+            expect(await md.getDelegateBlockNumber(delegator.address)).equals(
+                blockNumber
+            )
+            const receipt2 = await successfulTransaction(
+                md.connect(delegator).clearDelegateTrie(delegator.address)
+            )
+            expect(await md.getDelegateRoot(delegator.address)).equals(
+                constants.HashZero
+            )
+            expect(await md.getDelegateBlockNumber(delegator.address)).equals(
+                BigNumber.from('0')
+            )
+            /*
+             * await expect(
+             *     md.connect(delegator).getDelegateRoot(oracle.address)
+             * ).to.be.revertedWith('Ownable: caller is not the owner')
+             */
+        })
+    })
+
+    /* eslint-disable no-lone-blocks */
+
+    let admin: SignerWithAddress
+    let oracle: SignerWithAddress
+    let delegator: SignerWithAddress
+    let delegator2: SignerWithAddress
+    let md: MerkleDelegation
+})
