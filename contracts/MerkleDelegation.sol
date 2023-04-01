5ccadbc269f697d58f0d5962e630438493976aad
grezle <4310551+grezle@users.noreply.github.com>
2023-04-01 13:58:34 +0100

    fix: drop unused code and emit all delegate changes via SetDelegates

--- contracts/MerkleDelegation.sol
+++ contracts/MerkleDelegation.sol
@@ -10,11 +10,6 @@ struct DelegatorRecord {
     uint256 blockNumber;
 }
 
-struct DelegateeRecord {
-    address delegatee;
-    uint256 percentage; // 100,000 is 100%, 1,000 is 1%.
-}
-
 // https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
 // https://docs.openzeppelin.com/contracts/4.x/api/security#Pausable
 // https://docs.openzeppelin.com/contracts/4.x/api/utils#MerkleProof
@@ -23,44 +18,29 @@ contract MerkleDelegation is Ownable, Pausable {
     mapping(address => DelegatorRecord) public delegation;
     mapping(address => address) public delegateToDelegator;
 
-    event OwnerInitialized(address owner);
-    event OwnerChanged(address newOwner);
     event SetDelegates(
         address indexed delegator,
         bytes32 indexed trieRoot,
         uint256 blockNumber
     );
-    event ClearDelegate(address indexed delegator, bytes32 trieRoot);
 
     constructor(address _governanceToken) Ownable() Pausable() {
-        require(
-            _governanceToken != address(0),
-            "DR: token addr must be non-zero"
-        );
+        require(_governanceToken != address(0), "token addr must be non-zero");
         governanceToken = _governanceToken;
     }
 
-    //TODO: also the owner/admin can update the governance token address that is hashed with all leafs.
-    // function updateGovernanceToken(address newGovernanceToken) { governanceToken = newGovernancetoken; }
-
-    function setDelegateTrie(address delegator, bytes32 trieRoot)
-        external
-        whenNotPaused
-    {
-        require(delegator != address(0), "DR: delegator must be non-zero");
-        require(trieRoot != bytes32(0), "DR: trieRoot must be non-zero");
-        require(msg.sender == delegator, "DR: delegator must be msg.sender");
-        // Record to storage.
+    function setDelegateTrie(address delegator, bytes32 trieRoot) external whenNotPaused {
+        require(delegator == msg.sender, "delegator must be msg.sender");
+        require(trieRoot != bytes32(0), "trieRoot must be non-zero");
         delegation[delegator].trieRoot = trieRoot;
         delegation[delegator].blockNumber = block.number;
         emit SetDelegates(delegator, trieRoot, block.number);
     }
 
     function clearDelegateTrie(address delegator) external whenNotPaused {
-        require(delegator != address(0), "DR: delegator must be non-zero");
-        require(msg.sender == delegator, "DR: delegator must be msg.sender");
-        // Remove delegator record to save storage gas.
+        require(delegator == msg.sender, "delegator must be msg.sender");
         delete delegation[delegator];
+        emit SetDelegates(delegator, bytes32(0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421), block.number);
     }
 
     function pause() external onlyOwner whenNotPaused {
@@ -71,31 +51,17 @@ contract MerkleDelegation is Ownable, Pausable {
         _unpause();
     }
 
-    function getDelegateRoot(address delegator)
-        external
-        view
-        returns (bytes32 trieRoot)
-    {
-        require(delegator != address(0), "DR: delegator must be non-zero");
+    function getDelegateRoot(address delegator) external view returns (bytes32 trieRoot) {
+        require(delegator != address(0), "delegator must be non-zero");
         return delegation[delegator].trieRoot;
     }
 
-    function getDelegateBlockNumber(address delegator)
-        external
-        view
-        returns (uint256 blockNumber)
-    {
-        require(delegator != address(0), "DR: delegator must be non-zero");
-        return (delegation[delegator].blockNumber);
+    function getDelegateBlockNumber(address delegator) external view returns (uint256 blockNumber) {
+        require(delegator != address(0), "delegator must be non-zero");
+        return delegation[delegator].blockNumber;
     }
 
-    function transferOwnership(address newOwner)
-        public
-        virtual
-        override
-        onlyOwner
-        whenNotPaused
-    {
+    function transferOwnership(address newOwner) public virtual override onlyOwner whenNotPaused {
         _transferOwnership(newOwner);
     }
 }
